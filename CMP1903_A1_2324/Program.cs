7c6e6eaec6829899d1715dbd94149e4a017ad5f5
Morgan Loft <28074504@students.lincoln.ac.uk>
2024-03-05 13:17:04 +0000

    Final Submit

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -15,7 +15,6 @@ namespace CMP1903_A1_2324
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
             Game game = new Game(); // Creates an instance of the Game class.
             game.game(); // This calls the game method of the game class.
             Console.ReadLine(); // Prompts the user to press the Enter key so the console window doesn't instantly close.
